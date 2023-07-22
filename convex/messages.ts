586f69b2f9582a04c61057a2766e80d839772c3b
Ian Macartney <ianhmacartney@gmail.com>
2023-07-21 21:18:26 -0700

    1.0

--- convex/messages.ts
+++ convex/messages.ts
@@ -2,19 +2,19 @@ import { v } from "convex/values";
 import { Doc } from "./_generated/dataModel";
 import { internalMutation, internalQuery } from "./_generated/server";
 
-export const listAll = internalQuery(
-  async ({ db }): Promise<Doc<"messages">[]> => {
-    return await db.query("messages").collect();
-  }
-);
+export const listAll = internalQuery({
+  handler: async (ctx): Promise<Doc<"messages">[]> => {
+    return await ctx.db.query("messages").collect();
+  },
+});
 
 export const listByAuthor = internalQuery({
   args: { authorNumber: v.string() },
   handler: async (
-    { db },
+    ctx,
     { authorNumber }: { authorNumber: string }
   ): Promise<Doc<"messages">[]> => {
-    const messages = await db.query("messages").collect();
+    const messages = await ctx.db.query("messages").collect();
     return messages.filter((message) => message.author.includes(authorNumber));
   },
 });
@@ -24,11 +24,8 @@ export const send = internalMutation({
     body: v.string(),
     author: v.string(),
   },
-  handler: async (
-    { db },
-    { body, author }: { body: string; author: string }
-  ) => {
+  handler: async (ctx, { body, author }: { body: string; author: string }) => {
     const message = { body, author };
-    await db.insert("messages", message);
+    await ctx.db.insert("messages", message);
   },
 });
