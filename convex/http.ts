e6dab9d8f7aa9dd93d4c8e17087e35e118a6f59b
Sarah Shader <sarah@convex.dev>
2023-07-28 12:17:30 -0400

    Polish

--- convex/http.ts
+++ convex/http.ts
@@ -22,19 +22,20 @@ app.use(
   })
 );
 
-app.get("/hello", (c) => {
-  return c.text("hello!");
-});
-
 // Set up CORS as middleware
 app.use("/api/*", cors());
 
-// Nested routing -- This is `/api/hello`.
 // This endpoint should be accessible from the browser.
 const apiRouter = app.basePath("/api");
 
-apiRouter.get("/hello", (c) => {
-  return c.text("hello!");
+// Nested as `/api/listMessages
+apiRouter.get("/listMessages", async (c) => {
+  // Running a Convex query
+  const messages = await c.env.runQuery(internal.messages.listAll);
+
+  // Helpers for pretty JSON!
+  c.pretty(true, 2);
+  return c.json(messages);
 });
 
 apiRouter.get("/listMessages/:userId{[0-9]+}", async (c) => {
@@ -45,15 +46,6 @@ apiRouter.get("/listMessages/:userId{[0-9]+}", async (c) => {
   const messages = await c.env.runQuery(internal.messages.listByAuthor, {
     authorNumber: userId,
   });
-
-  // Helpers for pretty JSON!
-  c.pretty(true, 2);
-  return c.json(messages);
-});
-
-apiRouter.get("/listMessages", async (c) => {
-  // Running a Convex query
-  const messages = await c.env.runQuery(internal.messages.listAll);
 
   // Helpers for pretty JSON!
   c.pretty(true, 2);
@@ -76,18 +68,6 @@ apiRouter.post(
     await c.env.runMutation(internal.messages.send, { body, author });
     return c.text("Sent message!");
   }
-);
-
-app.use(
-  "/api2/*",
-  cors({
-    origin: "http://examplesite.come",
-    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
-    allowMethods: ["POST", "GET", "OPTIONS"],
-    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
-    maxAge: 600,
-    credentials: true,
-  })
 );
 
 export default new HttpRouterWithHono(app);
